f81fbdde54517cdd10b0d0306b5802c1023a8078
samkankrale <shubham@rudhisoft.com>
2026-01-15 08:26:49 +0530

    token changed

--- tokens.py
+++ tokens.py
@@ -1,7 +1,7 @@
 from jose import jwt, JWTError
 import datetime
 
-SECRET_KEY = "d2afc78c0a74354665e1f668a414af7f44a918c82f49184d39152d4221a2b130afa6a1e02488c0de2877c3295ff6872fdc8ac866f011d487fab67778bb0c0f39"
+SECRET_KEY = "b21ef1d6b02ebea15bb36009af894fe107d0a5734617114cfbc792cd2f2177edbb86bbfcdc7d97daca782be343620b52b67f1749768902b1b8e770f058896b0d"
 ALGORITHM = "HS256"
 def create_token(data : dict, expires_delta : int = 1000):
     to_encode = data.copy()
